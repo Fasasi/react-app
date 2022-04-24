c1bca314ca860b95435dbc4c3174cd51e024c4ac
fasasi <fasasitobi82@gmail.com>
2022-04-24 11:01:49 +0100

    edit version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Home from "./components/Home";
-import style from "./style.css";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyPage from "./components/MyPage";
 import Ecourses from "./components/Ecourses";
@@ -24,7 +24,7 @@ const App = () => {
   });
 
   return (
-    <Router className="maxwidth">
+    <Router>
       <Nav />
       <Routes>
         <Route path="/"  className="homepage" element={<Home />} />
